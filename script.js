ecb5c1885fed1f860c7c5e33e4ab643c7642c97a
mizaniewicz <mizaniewicz@gmail.com>
2017-11-21 11:13:26 +0100

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://blooming-bayou-50598.herokuapp.com/v1/';
-  const trelloApiRoot = 'https://blooming-bayou-50598.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://hidden-reaches-27383.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://hidden-reaches-27383.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
